e407ff12034b9c55ee39474e3ab950e04c8e033e
narangnaman29 <narangnaman29@gmail.com>
2024-09-01 21:40:09 +0530

    Initial commit

---
+++ components/component_style/style_css.py
@@ -0,0 +1,15 @@
+"""
+CSS for custom styling
+
+Author: Naman Narang
+Github: https://github.com/narangnaman29
+"""
+
+PADDING_TOP = """
+<style>
+        .main > div {
+            padding-top: 2rem;
+            padding-bottom: 2rem;
+        }
+    </style>
+"""
